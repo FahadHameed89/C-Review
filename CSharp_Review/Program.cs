93ed4e43c35fde1311ae5109559dbb29a5eab42d
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-19 09:59:28 -0600

    Talking about data Types;

--- CSharp_Review/Program.cs
+++ CSharp_Review/Program.cs
@@ -7,13 +7,28 @@ namespace CSharp_Review
         static void Main(string[] args)
         {
             // Declaration of a variable
-            string name;
+
+            char aChar; // A single character value
+            string aString; // String (A collection of characters))
+
+            bool aBoolean; // Boolean (True or False) values
+
+            byte aByte; // Super small integer values (120 max)
+            short aShortInt // Small integer values (~65000 max)
+            int anInt; // Numerical values with no decimals (~210000000 max)
+            long aLongInt // Big integer values (Bigger than above)
+
+            float aFloat; // floating point values with less precision than doubles
+            double aDouble; // Decimal values
+            decimal aDecimal; // Also decimal values
+
+            // Input from the Console
+            aString = Console.ReadLine();
 
             // Entry of the C# Program
             Console.WriteLine("Hello World! What's good? iT'S ALL gOOD!");
 
-            // Input from the Console
-            name = Console.ReadLine();
+
 
             // Exit of the C# Program
         }
