0a66ba23e11e7c0d7f3713f134d345b587e155e4
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-19 09:36:39 -0600

    Changed output message;

--- CSharp_Review/Program.cs
+++ CSharp_Review/Program.cs
@@ -6,7 +6,7 @@ namespace CSharp_Review
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! What's good?");
         }
     }
 }
